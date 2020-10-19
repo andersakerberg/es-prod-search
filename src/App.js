d772bee5a2810e0e1c836b9295ed2643ac79b594
Anders Åkerberg <anders.akerberg@ikea.com>
2020-10-19 22:32:15 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
-
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Image, CardMedia } from "@material-ui/core";
 import {
   ErrorBoundary,
   Facet,
@@ -12,7 +13,7 @@ import {
   ResultsPerPage,
   Paging,
   Sorting,
-  WithSearch
+  WithSearch,
 } from "@elastic/react-search-ui";
 import { Layout } from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
@@ -23,31 +24,59 @@ import {
   buildSearchOptionsFromConfig,
   buildSortOptionsFromConfig,
   getConfig,
-  getFacetFields
+  getFacetFields,
 } from "./config/config-helper";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 const { hostIdentifier, searchKey, endpointBase, engineName } = getConfig();
 const connector = new AppSearchAPIConnector({
   searchKey,
   engineName,
   hostIdentifier,
-  endpointBase
+  endpointBase,
 });
 const config = {
   searchQuery: {
     facets: buildFacetConfigFromConfig(),
-    ...buildSearchOptionsFromConfig()
+    ...buildSearchOptionsFromConfig(),
   },
   autocompleteQuery: buildAutocompleteQueryConfig(),
   apiConnector: connector,
-  alwaysSearchOnInitialLoad: true
+  alwaysSearchOnInitialLoad: true,
 };
 
 export default function App() {
+  const classes = useStyles();
   return (
     <SearchProvider config={config}>
-      <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
-        {({ wasSearched }) => {
+      <WithSearch
+        mapContextToProps={({
+          wasSearched,
+          searchTerm,
+          setSearchTerm,
+          results,
+        }) => ({ wasSearched, searchTerm, setSearchTerm, results })}
+      >
+        {({ wasSearched, searchTerm, setSearchTerm, results }) => {
+          const objArray = [];
+          results.forEach((row) => {
+            if (objArray[row.productidentifier.raw]) {
+              objArray[row.productidentifier.raw].value.push(row);
+            } else {
+              objArray[row.productidentifier.raw] = {
+                key: row.productidentifier.raw,
+                value: [row],
+              };
+            }
+          });
+          console.log(objArray);
           return (
             <div className="App">
               <ErrorBoundary>
@@ -61,17 +90,66 @@ export default function App() {
                           sortOptions={buildSortOptionsFromConfig()}
                         />
                       )}
-                      {getFacetFields().map(field => (
-                        <Facet key={field} field={field} label={field} />
-                      ))}
+                      {getFacetFields().map((field) => {
+                        console.log(field);
+                        return (
+                          <Facet
+                            key={field}
+                            field={field}
+                            label={field.substring(0, field.ength - 2)}
+                          />
+                        );
+                      })}
                     </div>
                   }
                   bodyContent={
-                    <Results
-                      titleField={getConfig().titleField}
-                      urlField={getConfig().urlField}
-                      shouldTrackClickThrough={true}
-                    />
+                    <div>
+                      {Object.keys(objArray).map(function (key) {
+                        const productsInGroup = objArray[key];
+                        const alreadyLoopedMerchants = [];
+                        const product = productsInGroup.value[0];
+                        return (
+                          <div>
+                            <div>{decodeURIComponent(product.name.raw)}</div>
+                            <div>
+                              <img
+                                src={decodeURIComponent(product.image.raw)}
+                                alt="image"
+                                width="200px"
+                                height="200px"
+                              />
+                            </div>
+                            <div>
+                              {productsInGroup.value.map(function (product) {
+                                if (
+                                  !alreadyLoopedMerchants.includes(
+                                    product.merchant.raw
+                                  )
+                                ) {
+                                  alreadyLoopedMerchants.push(
+                                    product.merchant.raw
+                                  );
+                                  return (
+                                    <Button
+                                      key={product.merchant.raw}
+                                      href={decodeURIComponent(product.url.raw)}
+                                      variant="outlined"
+                                      color="primary"
+                                    >
+                                      {product.merchant.raw + " "}
+                                      {product.price.raw.substring(
+                                        0,
+                                        product.price.raw.length - 2
+                                      ) + "SEK"}
+                                    </Button>
+                                  );
+                                }
+                              })}
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </div>
                   }
                   bodyHeader={
                     <React.Fragment>
